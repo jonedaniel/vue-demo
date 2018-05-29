bcc85a208f3c10633160d8d5b3a3c40623f492b4
zmh <623299342@qq.com>
2018-05-29 15:28:07 +0800

    new stage

---
+++ my-project/src/components/MyVueHello.vue
@@ -0,0 +1,9 @@
+
+
+<template>
+
+<div>
+    <h1>this is your first vue template!</h1>
+</div>
+
+</template>
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MyVueHello from '@/components/MyVueHello'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path:'/',
+      name:'MyVueHello',
+      component:MyVueHello
+    },
+    {
+      path: '/otherHello',
       name: 'HelloWorld',
       component: HelloWorld
     }
